1c7097765535d4bfc1b1e9f479687a2411e64020
Laís Moraes <lais.moraes@ascsolutions.com.br>
2021-11-13 22:29:28 -0300

    Ajustes banco e CRUD

--- Xamarin-CEP/Xamarin-CEP/ViewModels/ListPageViewModel.cs
+++ Xamarin-CEP/Xamarin-CEP/ViewModels/ListPageViewModel.cs
@@ -4,18 +4,22 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin_CEP.Models;
+using Xamarin_CEP.Views;
 
 namespace Xamarin_CEP.ViewModels
 {
     public class ListPageViewModel : BaseViewModel
     {
+        private Endereço seletedAddress;
         public ObservableCollection<Endereço> Endereços { get; }
         public Command LoadEndereçosCommand { get; }
+        public Command<Endereço> EndereçoSelecionado { get; }
 
         public ListPageViewModel()
         {
             Endereços = new ObservableCollection<Endereço>();
             LoadEndereçosCommand = new Command(async () => await ExecuteLoadItemsCommand());
+            EndereçoSelecionado = new Command<Endereço>(OnSelectedAddress);
         }
 
         async Task ExecuteLoadItemsCommand()
@@ -41,9 +45,28 @@ namespace Xamarin_CEP.ViewModels
             }
         }
 
+        public Endereço SelectedAddress
+        {
+            get => seletedAddress;
+            set
+            {
+                SetProperty(ref seletedAddress, value);
+                OnSelectedAddress(value);
+            }
+        }
+
         public void OnAppearing()
         {
             IsBusy = true;
+        }
+
+        async void OnSelectedAddress(Endereço endereço)
+        {
+            if (endereço == null)
+                return;
+
+            // This will push the ItemDetailPage onto the navigation stack
+            await Shell.Current.GoToAsync($"{nameof(EndereçoPage)}?{nameof(EndereçoPageViewModel.CEP)}={endereço.CEP}");
         }
 
     }
